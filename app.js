2440dd018717540c8d57e9ace92132a683f627ac
Shivanshu-GITH <shivanshu.um@gmail.com>
2026-02-12 18:51:55 +0530

    Setting up the basic listing Schema.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const app= express();
 const mongoose= require("mongoose");
+const Listing= require("../stayfinder-mern/models/listing.js");
 
 MONGO_URL="mongodb://127.0.0.1:27017/stayfinder";
 
@@ -17,6 +18,19 @@ async function main() {
 app.get("/", (req, res)=>{
     res.send("Hi, I am root");
 });
+
+app.get("/testListing", async (req, res)=>{
+    let sampleListing = new Listing({
+        title: "My New Villa", 
+        description: "By the beach",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+    });
+    await sampleListing.save();
+    console.log("Sample was saved.");
+    res.send("Successful testing");
+})
 
 app.listen(8080, ()=>{
     console.log("Server is listening to port 8080");
