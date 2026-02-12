bce61cfb2f296babba0cfd3cbb04a57f5a424351
Shivanshu-GITH <shivanshu.um@gmail.com>
2026-02-12 19:12:22 +0530

    Initialized the database for listings.

--- models/listing.js
+++ models/listing.js
@@ -2,16 +2,20 @@ const mongoose= require("mongoose");
 const Schema= mongoose.Schema;
 
 const listingSchema = new Schema({
-    title : {
+    title: {
         type: String,
         required: true,
     },
     description: String,
     image: {
-        type: String,
-        default: "https://unsplash.com/https://plus.unsplash.com/premium_photo-1687960116497-0dc41e1808a2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dphotos/two-chairs-sitting-in-front-of-a-swimming-pool-k_My4rXk4Lc",
-        
-        set: (v) => v===""?"https://unsplash.com/https://plus.unsplash.com/premium_photo-1687960116497-0dc41e1808a2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dphotos/two-chairs-sitting-in-front-of-a-swimming-pool-k_My4rXk4Lc": v,
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?auto=format&fit=crop&w=800&q=60"
+        }
     },
     price: Number,
     location: String,
