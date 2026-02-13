d6625493ce86502866e1207f5f9db50d05b15c0e
Shivanshu-GITH <shivanshu.um@gmail.com>
2026-02-13 19:28:33 +0530

    Created Navbar, Footer, Boilerplate layout and applied the basic styling to all the pages.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose= require("mongoose");
 const Listing= require("../stayfinder-mern/models/listing.js");
 const path= require("path");
 const methodOverride= require("method-override");
+const ejsMate= require("ejs-mate");
 
 MONGO_URL="mongodb://127.0.0.1:27017/stayfinder";
 
@@ -21,7 +22,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
-
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res)=>{
     res.send("Hi, I am root");
@@ -31,7 +33,7 @@ app.get("/", (req, res)=>{
 //Index Route
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
+    res.render("listings/index", { allListings });
 });
 
 //Route for the creation of new list: includes new and the create route...
@@ -55,7 +57,7 @@ app.post("/listings", async (req, res) => {
 app.get("/listings/:id/edit", async (req, res)=>{
     let {id}= req.params;
     const listing= await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
+    res.render("listings/edit", {listing});
 });
 
 //Update Route
@@ -78,7 +80,7 @@ app.delete("/listings/:id", async (req, res)=>{
 app.get("/listings/:id", async (req, res)=>{
     let {id}= req.params;
     const listing= await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+    res.render("listings/show", {listing});
 });
 
 app.listen(8080, ()=>{
